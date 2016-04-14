089904fdf61263f448d68e3d5b016afd5e46c5e6
duykhoa <duykhoa12t@gmail.com>
2016-04-14 11:04:24 +0800

    Add script to update

--- lib/benchmark_es.rb
+++ lib/benchmark_es.rb
@@ -13,12 +13,18 @@ module BenchmarkEs
   extend AllIds
 end
 
-printf "Start index %s \n", Time.now
-BenchmarkEs.index_flat_data(nums: 50)
-printf "Finish index %s \n-----\n", Time.now
+def main
+  #printf "Start index %s \n", Time.now
+  #BenchmarkEs.index_flat_data(nums: 50)
+  #printf "Finish index %s \n-----\n", Time.now
 
-printf "Start list all ids %s \n", Time.now
-puts BenchmarkEs.all_ids(batch_size: 5000)
-printf "Finish list all ids %s \n", Time.now
+  printf "[%s] Start list all ids \n", Time.now
+  ids = BenchmarkEs.all_ids(batch_size: 5000)
+  printf "[%s] Finish list all ids \t Total %d \n-----\n", Time.now, ids.count
 
-#BenchmarkEs.update_info(nums: 50)
+  printf "[%s] Start updating all the info\n", Time.now
+  BenchmarkEs.update_info(ids: ids, batch_size: 5000)
+  printf "[%s] Finish updating\n-----\n", Time.now
+end
+
+main
--- lib/es/actions/update.rb
+++ lib/es/actions/update.rb
@@ -1,29 +1,26 @@
 module Update
-  YEARS = 365
-
-  def update_info(ids:)
-    Mapper.bulk(update_data(nums))
+  def update_info(ids:, batch_size:)
+    ids.each_slice(batch_size) do |slice|
+      Mapper.bulk(update_data(slice))
+    end
   end
 
-  def update_data(nums)
-    (0...nums).inject([]) do |r, id|
-      (0...365).each do |x|
-        d =
-          {
-          update:
-          {
-            _id: id,
-            data: {
-              doc: {
-                max_guest: 11
-              }
-            }
+  def update_data(slice)
+    slice.map do |id|
+      sample_update_data(id)
+    end
+  end
+
+  def sample_update_data(id)
+    {
+      update: {
+        _id: id,
+        data: {
+          doc: {
+            max_guest: 11
           }
         }
-
-        r << d
-      end
-      r
-    end
+      }
+    }
   end
 end
